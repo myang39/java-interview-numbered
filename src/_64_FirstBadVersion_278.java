95ea3463c9b2e69eba0a08d62cbd32f97791f539
Minkang Yang <minkangyang@gmail.com>
2019-07-08 21:38:41 -0700

    problem 64

---
+++ src/_64_FirstBadVersion_278.java
@@ -0,0 +1,22 @@
+/**
+ * Created by minkangyang on 21:37 Mon Jul, 2019
+ * https://leetcode.com/problems/first-bad-version/
+ */
+public class _64_FirstBadVersion_278 {
+//    public int firstBadVersion(int n) {
+//        int left = 1, right = n;
+//        while (left < right) {
+//            int mid = left + (right - left) / 2;
+//            if (!isBadVersion(mid)) {
+//                left = mid + 1;
+//            } else {
+//                right = mid;
+//            }
+//        }
+//        if (isBadVersion(right)) {
+//            return right;
+//        } else {
+//            return left;
+//        }
+//    }
+}
